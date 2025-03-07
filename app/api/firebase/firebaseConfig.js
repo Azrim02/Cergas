ed9e1af2ca201bfbd35099be47c32784c002af45
Azrim02 <mirzashariman02@gmail.com>
2025-03-07 15:13:50 +0000

    implemented auth persistence

--- app/api/firebase/firebaseConfig.js
+++ app/api/firebase/firebaseConfig.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDbj58lqSvjFEz4ODlThXyJqUR_kyxohQo",
@@ -16,7 +17,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(app);
+
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 const db = getFirestore(app); 
 
 export { auth, db };
